91fc78140c082e61ed01700098da44d9301bc150
skatzrskx55q <skatzr1997@gmail.com>
2026-05-04 13:36:05 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -9,7 +9,7 @@ import requests
 
 MODEL_ID = "skatzR/USER-BGE-M3-MiniLM-L12-v2-Distilled"
 GITHUB_TXT_URLS: List[str] = [
-    "https://raw.githubusercontent.com/<skatzrskx55q>/<LH>/<main>/<Документ>.txt",
+    "https://raw.githubusercontent.com/skatzrskx55q/LH/main/Документ.txt",
 ]
 REQUEST_TIMEOUT_SECONDS = 30
 
